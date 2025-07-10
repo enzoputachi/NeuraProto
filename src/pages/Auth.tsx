c2a1302a1827d6ff7629686e3bd68bd0b863dc96
Enzoputachi <samuelnzoputachi@gmail.com>
2025-07-10 09:44:17 +0100

    remove lock

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,4 +1,4 @@
-
+,
 import React, { useState } from 'react';
 import Login from '@/components/auth/Login';
 import Signup from '@/components/auth/Signup';
@@ -14,11 +14,11 @@ const Auth = () => {
     <div className="min-h-screen bg-gray-50 flex items-center justify-center px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
         <div className="bg-white rounded-2xl shadow-elegant p-8">
-          {isLoginMode ? (
+          {/* {isLoginMode ? (
             <Login onToggleMode={toggleMode} />
           ) : (
             <Signup onToggleMode={toggleMode} />
-          )}
+          )} */}
         </div>
       </div>
     </div>
