64d851b44e69d5754acddd19cb3386178de3a2c5
Enzoputachi <samuelnzoputachi@gmail.com>
2025-07-14 00:14:57 +0100

    update

--- src/components/DetailsSection.tsx
+++ src/components/DetailsSection.tsx
@@ -3,34 +3,40 @@ import { useInView } from "react-intersection-observer";
 
 const sections = [
   {
-    title: "Stocks",
-    text:
-      "Stocks are parts of a company that you can own. When you buy a stock, you own a small piece of that company.",
-    bgImage: "/stocks.jpeg",
-  },
-  {
     title: "Gold",
     text:
       "Gold is a valuable metal used to protect money from losing value. People invest in it during tough economic times.",
-    bgImage: "/gold.jpeg",
+    bgImage: "/gold.png",
+  },
+    {
+    title: "Stocks",
+    text:
+      "Stocks are parts of a company that you can own. When you buy a stock, you own a small piece of that company.",
+    bgImage: "/stocks.png",
   },
   {
+    title: "Commodities",
+    text:
+      "Commodities are raw materials like oil, gas, and wheat. Investing in them can protect against inflation and market changes.",
+    bgImage: "/seeds.png",
+  },
+  {
+    title: "REITs (Real Estate Investment Trusts)",
+    text:
+      "These are special stocks that allow you invest in companies that manage properties and tenancy.",
+    bgImage: "/reit.png",
+  },
+    {
     title: "ETFs (Exchange-Traded Funds)",
     text:
       "ETFs are bundles of assets like stocks or bonds that you can buy and sell like a single stock. They help spread risk.",
-    bgImage: "/ETFS.jpeg",
+    bgImage: "/etfs.png",
   },
   {
     title: "Bonds",
     text:
       "Bonds are loans to companies or governments. In return, they pay you interest over time and give your money back later.",
     bgImage: "/Bonds.jpeg",
-  },
-  {
-    title: "Commodities",
-    text:
-      "Commodities are raw materials like oil, gas, and wheat. Investing in them can protect against inflation and market changes.",
-    bgImage: "/Seeds.jpg",
   },
 ];
 
