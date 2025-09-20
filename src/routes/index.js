c462efa67e04bec16937a0277512a819b659aaa5
Enzoputachi <samuelnzoputachi@gmail.com>
2025-09-20 10:44:37 +0100

    Update routes index.js to include new module endpoints

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import userAuthRoutes from './userAuthRoutes.js';
-
-
+import planRoutes from './planRoutes.js';
+import paymentRoutes from './paymentRoutes.js';
+import contentRoutes from './contentRoutes.js';
 
 const router = express.Router();
 
 router.use('/users', userAuthRoutes);
-
+router.use('/plans', planRoutes);
+router.use('/payments', paymentRoutes);
+router.use('/content', contentRoutes);
 
 export default router;
